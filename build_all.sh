ae11404fa1e730a281e43b197088f42d068cbfb8
luoye <84152821@qq.com>
2022-01-09 17:55:46 -0800

    update the build sh,not rm preview build

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 chmod -R 777 ./
-make clean
-make uninstall
-rm -rf $(pwd)/android
+#make clean
+#make uninstall
+#rm -rf $(pwd)/android
 ./build_config.sh armv7-a
 ./build_config.sh armv8-a
 #./build_config.sh x86
